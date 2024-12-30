679d7d8ea33b9f398060b38fa212b439fccea079
krishnagkashyap <krishnagkashyap@gmail.com>
2024-12-30 10:39:53 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,17 @@ public class Main {
 
                 if (input.startsWith("echo")) {
                     System.out.println(input.substring("echo".length() + 1, input.length()));
+                } else if (input.startsWith("type")) {
+                    String lit0 = input.substring("type".length() + 1, input.length());
+                    if (lit0.equalsIgnoreCase("echo")) {
+                        System.out.println("echo is a shell builtin");
+                    } else if (lit0.equalsIgnoreCase("exit")) {
+                        System.out.println("exit is a shell builtin");
+                    } else if (lit0.equalsIgnoreCase("type")) {
+                        System.out.println("type is a shell builtin");
+                    } else {
+                        System.out.println(lit0 + ": not found");
+                    }
                 } else {
                     System.out.println(input + ": command not found");
                 }
