9b2b5191eeb0805c0cd9489856080fc4af3b40e0
krishnagkashyap <krishnagkashyap@gmail.com>
2025-01-08 18:01:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,28 @@ import java.util.Arrays;
 import java.io.*;
 
 public class Main {
+    public static void println(String str) {
+
+        System.out.println(str);
+    }
+
+    public static void print(String str) {
+
+        System.out.print(str);
+    }
+
     public static void notFound(String str) {
 
         System.out.println(str + ": not found");
     }
 
     public static void debugOn(String str) {
-
         System.out.printf("%s%n", str);
+    }
+
+    public static String getCwd(String command) throws Exception {
+        File pwd = new File(command);
+        return pwd.getCanonicalFile().getParent();
     }
 
     public static String getPath(String typeString) {
@@ -94,24 +108,34 @@ public class Main {
     }
 
     public static void executeCommand(String input) throws Exception {
+        // pass the entire command because the command might come with its own arguments
         String command = input.split(" ")[0];
+        // String commandArgs = input.split(" ")[1];
         // debugOn(command);
+        // debugOn(commandArgs);
         String path = getPlainPath(command);
         File fPath = new File(path);
         // debugOn(path);
         if (command.equalsIgnoreCase("pwd")) {
-            File pwd = new File(command);
-            // System.out.println(pwd.getAbsolutePath());
-            // System.out.println(pwd.getCanonicalPath());
-            // System.out.println(pwd.getName());
-            // System.out.println(pwd.getParent());
-            System.out.println(pwd.getCanonicalFile().getParent());
-            // System.out.println(pwd.getPath());
+            String cmd = "pwd";
+            String printPWD = getCwd(cmd);
+            System.out.println(printPWD);
 
         } else if (!fPath.isFile()) {
-            System.out.println(command + ": command not found");
+            println(command + ": command not found");
         } else {
-            String fullPath = path + input.substring(command.length());
+            // println("HELLO AM HERE");
+            String commandArgs = input.split(" ")[1];
+            // println("IS TERE AN ARG");
+            // println(commandArgs);
+            // debugOn(command);
+            // debugOn(commandArgs);
+            String path0 = getPath(command);
+            File fPath0 = new File(path0);
+
+            String fullPath = (!commandArgs.equalsIgnoreCase("")) ? command + " " + commandArgs
+                    : path + input.substring(command.length());
+
             // debugOn(fullPath);
             // System.out.println(Arrays.toString(fullPath.split(" ")));
             Process p = Runtime.getRuntime().exec(fullPath.split(" "));
@@ -125,27 +149,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("$ ");
+            print("$ ");
             String input = scanner.nextLine();
 
             if (!input.equalsIgnoreCase("exit 0")) {
 
                 if (input.startsWith("echo")) {
 
-                    System.out.println(input.substring("echo".length() + 1, input.length()));
+                    println(input.substring("echo".length() + 1, input.length()));
 
                 } else if (input.startsWith("type")) {
 
                     String typeString = input.substring("type".length() + 1, input.length());
 
                     if (Arrays.asList(built_in_commands).contains(typeString)) {
-                        System.out.println(typeString + " is a shell builtin");
+                        println(typeString + " is a shell builtin");
                         // break;
 
                     } else {
                         // debugOn("BEFORE PATH");
                         String typePath = getPath(typeString);
-                        System.out.println(typePath);
+                        println(typePath);
                         // debugOn("AFTER PATH" + typePath);
                     }
                 } else {
--- your_program.sh
+++ your_program.sh
@@ -21,4 +21,5 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
+# print java_home as debug mode env is enabled
 exec java -jar /tmp/codecrafters-build-shell-java/codecrafters-shell.jar "$@"
